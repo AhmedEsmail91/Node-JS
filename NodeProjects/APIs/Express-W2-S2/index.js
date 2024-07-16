97589c8174c2dbefcd184e9da70444286ad35552
Ahmed Esmail <107895995+AhmedEsmail91@users.noreply.github.com>
2024-07-16 23:07:21 +0300

    CRUD Operations on Array of Objects (Mainly) summay: - CRUD Operations on Array of Objects - Some knowledge on Middlewares - Route.use() vs Route.all()

--- NodeProjects/APIs/Express-W2-S2/index.js
+++ NodeProjects/APIs/Express-W2-S2/index.js
@@ -1,19 +1,19 @@
 const express = require("express");
 // Express is a function
-const app=express();
-// app is an object
-app.listen(3000,()=>{
+const Route=express();
+// Route is an object
+Route.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });
 // GET request
-app.get("/",(req,res)=>{
+Route.get("/",(req,res)=>{
     res.send("Hello World");
 });
-app.get("/about",(req,res)=>{
+Route.get("/about",(req,res)=>{
     res.setHeader("Content-Type","text/html");
     res.send("<h1>About Us</h1>");
 });
-const FakeData=
+Users=
 [
   {
     "id": 1,
@@ -106,16 +106,12 @@ const FakeData=
     "occupation": "Product Manager"
   }
 ]
-app.get("/api",(req,res)=>{
-    res.json(FakeData); // instead of res.end(JSON.stringify(FakeData));
+Route.get("/api",(req,res)=>{
+    res.json(Users); // instead of res.end(JSON.stringify(Users));
 });
-app.get('/file',(req,res)=>{
-    res.sendFile(__dirname+"/index.html");
-})
-/* through id
-
-app.get("/api/:id",(req,res)=>{
-    const user=FakeData.find(
+/* get user by id
+Route.get("/api/:id",(req,res)=>{
+    const user=Users.find(
         (x)=>x.id===parseInt(req.params.id)
     );
     if(!user){
@@ -124,3 +120,133 @@ app.get("/api/:id",(req,res)=>{
     res.json(user);
 });
 */
+
+Route.get('/file',(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+})
+
+//POST
+/*
+Route.post("/addUser",(req,res)=>{
+    // req.on('data',(chunck)=>{
+    //     console.log(chunck.toString());
+    // });
+    console.log(req.body);
+    res.send("User added successfully");
+});
+*/
+Route.use(express.json()); // this will be applied (fire) to all the routes (get, post, delete, put, patch) in the application but must be decleared before the routes.
+// POST+Middleware
+Route.post("/addUser",(req,res)=>{
+    // express.json() is a middleware that parses the incoming request with JSON payloads, it's like (urlencoded) for Template engines SSR
+    
+    console.log(req.body);
+    // req.body==$request in php (it's an object) carrys the data from the client
+    
+    // OR SIMPLY:
+    
+    const {email}=req.body; // destructuring
+    let isExist=Users.find((user)=>user.email===email);
+
+    if(isExist){
+      res.json({message:"Email already exists"});
+    }
+    else {
+      const newUser={
+        "id":Users.length+1,
+        "name":req.body.name,
+        "age":req.body.age,
+        "email":req.body.email,
+        "phone":req.body.phone,
+        "address":req.body.address,
+        "occupation":req.body.occupation
+      };  
+      Users.push(newUser);
+      res.json({message:"User added successfully"});
+    }
+
+    // Users.push(req.body);
+    
+});
+
+Route.delete("/deleteUser/:id",(req,res)=>{
+    const userIndex=Users.findIndex(
+        (user)=>user.id===parseInt(req.params.id) //req.params.id , params is an object that contains the route parameters like: /:id
+    );
+    // array.findIndex((value,index,array)=>{//---Manipulation---//}) => return -1 if the element not found in the array.
+    if(userIndex < 0){
+        res.status(404).send("User not found");
+    }
+    //array.splice(start,deleteCount).
+    //start: index at which to start changing the array,
+    //deleteCount: An integer indicating the number of elements in the array to remove from start.
+    let deletedUser=Users.splice(userIndex,1);
+
+    res.json({message:"User deleted successfully",deletedUser});
+});
+
+Route.put("/updateUser/:id",(req,res)=>{
+    const userIndex=Users.findIndex(
+        (user)=>user.id===parseInt(req.params.id)
+    );
+    
+    if(userIndex < 0){
+        res.status(404).send("User not found");
+    }
+
+    const updatedUser={
+      "id":parseInt(req.params.id),
+      "name":req.body.name,
+      "age":req.body.age,
+      "email":req.body.email,
+      "phone":req.body.phone,
+      "address":req.body.address,
+      "occupation":req.body.occupation
+    };  
+    Users[userIndex]=updatedUser;
+    res.json({message:"User updated successfully"});
+});
+
+//Middleware
+// Structure of Node Middlewares : https://bit.ly/4bFAKHa
+// how to add the middlewares in the route
+Route.get('/middlewares_testing_1',
+  (req,res,next)=>{res.send('Done1.0')}/*middleware 1*/, // the res.send() will stop the execution of the next middlewares so we need to use next() to pass the control to the next middleware.
+  (req,res,next)=>{res.send('Done2.0')}/*middleware 2*/,
+  (req,res,next)=>{res.send('Done3.0')}/*middleware 3*/);
+
+Route.get('/middlewares_testing_2',
+  (req,res,next)=>{next()}/*middleware 1*/, // the next middleware will be executed after the current middleware after using next().
+  (req,res,next)=>{next()}/*middleware 2*/,
+  (req,res,next)=>{next()}/*middleware 3*/, 
+  (req,res,next)=>{res.json(Users)}/*middleware 4*/
+);
+
+// Assigning the middlewares to a variable.
+
+let isLoggedin=true;
+
+let auth=(req,res,next)=>{
+  if(isLoggedin){
+    next();
+  }
+  else{
+    res.send("Can't Login.")
+  }
+}
+Route.get('/auth_test',
+  auth,
+  (req,res,next)=>{res.json(Users)}
+);
+
+// Handling the Buffer instead of calling express.json() middleware:
+// check line: 138.
+
+/* Route.all vs Route.use
+ *Route.use():
+  app.use('/api', authenticateMiddleware); // Applies to all HTTP methods
+  app.use('/api', logRequestMiddleware); // Runs after authentication for any HTTP method
+ *Route.all():
+  app.all('/api/resource', (req, res) => {
+  *Logic here will execute for any HTTP method (GET, POST, etc.)
+});*/
