6b5a211b25c489614c23d53ea9f64be92c309d56
Ahmed Esmail <107895995+AhmedEsmail91@users.noreply.github.com>
2024-07-28 02:43:21 +0300

    Adding prefix to a group of routes

--- NodeProjects/APIs/Sequelize/index.js
+++ NodeProjects/APIs/Sequelize/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import {userRouter} from './src/modules/user/user.route.js';
 const app = express();
 app.use(express.json());
-//adding prefix to all routes
+//*adding prefix to all routes
 app.use('/api',userRouter);
 // app.use(userRouter);
 const port = 3000;
